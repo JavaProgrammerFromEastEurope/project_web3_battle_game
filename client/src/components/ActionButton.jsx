a7939b1af8a2846a45afc9bda6c8ed5a40f8646a
vm <gordonfree.com@gmail.com>
2022-11-01 12:31:59 +0300

    update battle page

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const ActionButton = () => {
-  return (
-    <div>
+import styles from '../styles';
 
+const ActionButton = ({ imgUrl, handleClick, restStyles}) => {
+  return (
+    <div className={`
+        ${styles.gameMoveBox}
+        ${styles.flexCenter}
+        ${styles.glassEffect}
+        ${restStyles}`}>
+        <img
+          src={imgUrl}
+          alt="action_img"
+          className={styles.gameMoveIcon}
+          />
     </div>
   )
 }
